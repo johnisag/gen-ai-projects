adfaf5620934434e3100f3b5e339f26f66a83255
Ioannis Agorgianitis <iagorgia@emeal.nttdata.com>
2025-02-05 17:56:13 +0000

    init image caption

---
+++ image-caption-blip/image_cap.py
@@ -0,0 +1,30 @@
+import requests
+from PIL import Image
+from transformers import AutoProcessor, BlipForConditionalGeneration
+
+# Load the pretrained processor and model
+
+# It wraps a BLIP image processor and an OPT/T5 tokenizer into a single processor.
+processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+
+# It can generate text based on an input image and an optional piece of text. 
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+
+# Load your image, DONT FORGET TO WRITE YOUR IMAGE NAME
+img_path = "happy_dog.jpg"
+
+# convert it into an RGB format 
+image = Image.open(img_path).convert('RGB')
+
+# You do not need a question for image captioning
+text = "the image of"
+inputs = processor(images=image, text=text, return_tensors="pt")
+
+# Generate a caption for the image
+outputs = model.generate(**inputs, max_length=50)
+
+# Decode the generated tokens to text
+caption = processor.decode(outputs[0], skip_special_tokens=True)
+
+# Print the caption
+print(caption)
